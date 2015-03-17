e58d94aed2a45d739b5c204b18393b078cb4c194
Jonathan Kempf <kempfjj@gmail.com>
2015-03-17 07:08:51 -0400

    Adding the SCSS includes

---
+++ config.rb
@@ -0,0 +1,10 @@
+require "compass"
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascript"
+fonts_dir = "fonts"
+disable_warnings = true
+output_style = :compact
