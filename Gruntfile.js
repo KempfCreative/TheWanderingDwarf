5c1ab151d8737950ff37466defffeb219a73c1fe
Jonathan Kempf <kempfjj@gmail.com>
2015-03-16 22:41:18 -0400

    Removing extra packages not needed by the grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,103 +6,57 @@ var mountFolder = function(connect, dir) {
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        cssmin: {
-            task: {
-                src: ['source'],
-                dest: 'destination'
-            },
-            options: {
-                'banner': null,
-                'keepSpecialComments': '*',
-                'report': 'min'
-            }
-        },
-        sass: {
-            task: {
-                src: ['source'],
-                dest: 'destination'
-            },
-            options: {
-                'sourcemap': 'auto',
-                'trace': false,
-                'unixNewlines': false,
-                'check': false,
-                'style': 'nested',
-                'precision': 3,
-                'quiet': false,
-                'compass': false,
-                'debugInfo': false,
-                'lineNumbers': false,
-                'loadPath': [],
-                'require': [],
-                'cacheLocation': '.sass-cache',
-                'noCache': false,
-                'bundleExec': false,
-                'banner': '',
-                'update': false
-            }
-        },
         watch: {
-            task: {
-                src: ['source'],
-                dest: 'destination'
-            },
-            options: {
-                'spawn': true,
-                'interrupt': false,
-                'debounceDelay': 500,
-                'interval': 100,
-                'event': 'all',
-                'reload': false,
-                'forever': true,
-                'dateFormat': null,
-                'atBegin': false,
-                'livereload': false,
-                'cwd': process.cwd(),
-                'livereloadOnError': true
+            compass: {
+                files: ['sass/**/*.scss'],
+                tasks: ['compass']
             },
             livereload: {
-                files: ['inc/**/*.html', '*.html', 'css/*.css', 'js/**/*.js', 'img/**/*.{png,jpg,jpeg,webp}'],
-                tasks: ['concat:dist']
+                files: ['*.html', 'css/*.css', 'javascript/**/*.js', 'images/**/*.{png,jpg,jpeg,webp}']
             }
         },
         compass: {
-            task: {
-                src: ['source'],
-                dest: 'destination'
-            },
             options: {
-                'gigantocluster-of-options-see-them-here': 'https'
+                sassDir: 'sass',
+                cssDir: 'css',
+                imagesDir: 'img',
+                javascriptsDir: 'js',
+                fontsDir: 'fonts',
+                relativeAssets: true
+            },
+            debug: {
+                options: {
+                    debugInfo: true
+                }
             }
         },
         connect: {
-            task: {
-                src: ['source'],
-                dest: 'destination'
-            },
             options: {
-                'port': 8000,
-                'protocol': 'http',
-                'hostname': '0.0.0.0',
-                'base': '.',
-                'directory': null,
-                'keepalive': false,
-                'debug': false,
-                'livereload': false,
-                'open': false,
-                'useAvailablePort': false,
-                'onCreateServer': null,
-                'middleware': []
+                port: 8000,
+                protocol: 'http',
+                hostname: 'localhost',
+                base: ''
+            },
+            livereload: {
+                options: {
+                    middleware: function(connect) {
+                        return [
+                        mountFolder(connect, '')];
+                    }
+                }
+            }
+        },
+        open: {
+            local: {
+                path: 'http:localhost:<%= connect.options.port %>'
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-livereload');
 
-    grunt.registerTask('default', ['cssmin', 'sass', 'watch', 'compass', 'connect']);
+    grunt.registerTask('default', ['watch', 'compass', 'connect']);
 };
