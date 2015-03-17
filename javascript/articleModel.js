8d420baf5a8972321f9daf90c1097baee9d7e18a
Jonathan Kempf <kempfjj@gmail.com>
2015-03-17 03:03:17 -0400

    Moving the AJAX logic to the proper place in the model, now that we have the callback working

---
+++ javascript/articleModel.js
@@ -0,0 +1,25 @@
+TWD.articleModel = {};
+
+TWD.articleModel.loadArticles = function(){
+    var articleDOM = $(document),
+        articleTargets = articleDOM.querySelectorAll('[data-article-target]');
+
+    if(!TWD.articleView.articlesLoaded){
+        TWD.articleView.articlesLoaded = true;
+        for (var i = 1; i <= articleTargets.length; i++) {
+            (function(i){
+                $.ajax({
+                    type: 'GET',
+                    url: '/articles/mainArticle' + i + '.json',
+                    cache: false,
+                    success: function(data) {
+                        $(articleTargets[i]).append(data.success.articleImage);
+                        $(articleTargets[i]).append(data.success.articleTime);
+                        $(articleTargets[i]).append(data.success.articleHeading);
+                        $(articleTargets[i]).append(data.success.articleBody);
+                    }
+                });
+            })(i);
+        };
+    }
+};
